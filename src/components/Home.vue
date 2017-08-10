b69402eba018397cb526d87fbe4969e41b0970e9
marciocamello@outlook.com <netunia234>
2017-08-10 20:40:00 -0300

    First commit

---
+++ src/components/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container>
+    <v-layout row wrap class="mb-2">
+      <v-flex xs12 sm6 class="text-xs-center text-sm-right">
+        <v-btn large router to="/meetups" class="info">Explore Meetups</v-btn>
+      </v-flex>
+      <v-flex xs12 sm6 class="text-xs-center text-sm-left">
+        <v-btn large router to="/meetup/new" class="info">Organize Meetup</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item
+            v-for="(meetup,i) in meetups"
+            :src="meetup.imageUrl" :key="meetup.id">
+            <div class="title">{{ meetup.title}}</div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap class="mt-2">
+      <v-flex xs12 class="text-xs-center">
+        <p>Join our awesome meetups!</p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        meetups: [
+          {
+            imageUrl: 'http://www.hotel-discount.com/wp-content/uploads/New_york_times_square-terabass.jpg',
+            id: 'edfrgertert123',
+            title: 'Meetup in New York'
+          },
+          {
+            imageUrl: 'https://media.architecturaldigest.com/photos/5931758bf368f9234dedb534/master/pass/Paris_Personalities_GettyImages-546896176-2.jpg',
+            id: 'ertert23234234',
+            title: 'Meetup in Paris'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title{
+    position: absolute;
+    width: 100%;
+    text-align: center;
+    bottom:50px;
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-size: 2em;
+    padding: 20px;
+  }
+</style>
