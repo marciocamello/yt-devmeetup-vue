248b4a1ddd2b5ebf284553d7aaf9f8ac273d009e
marciocamello@outlook.com <netunia234>
2017-08-10 21:12:00 -0300

    change dist path

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../public_html/index.html'),
+    assetsRoot: path.resolve(__dirname, '../public_html'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     productionSourceMap: true,
