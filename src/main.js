7593568ef124b5b9eedd5c7490291700f84f5449
marciocamello@outlook.com <netunia234>
2017-08-13 10:34:01 -0300

    Add auth path

--- src/main.js
+++ src/main.js
@@ -19,11 +19,11 @@ new Vue({
   render: h => h(App),
   created () {
     firebase.initializeApp({
-      apiKey: 'AIzaSyATlMZPWXlRtQUs1qUNnG7GpaSrH4Xd6UE',
-      authDomain: 'api-project-440315309030.firebaseapp.com',
-      databaseURL: 'https://api-project-440315309030.firebaseio.com',
-      projectId: 'api-project-440315309030',
-      storageBucket: 'api-project-440315309030.appspot.com'
+      apiKey: '',
+      authDomain: '',
+      databaseURL: '',
+      projectId: '',
+      storageBucket: ''
     })
   }
 })
