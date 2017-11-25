f3d8506f1d83406792768dfa160dae88d466b192
marciocamello@outlook.com <netunia234>
2017-11-25 07:35:04 -0200

    merge

---
+++ src/auth/firebase.js
@@ -0,0 +1,10 @@
+/**
+ * Created by banda on 08/13/17.
+ */
+export default {
+  apiKey: 'AIzaSyATlMZPWXlRtQUs1qUNnG7GpaSrH4Xd6UE',
+  authDomain: 'api-project-440315309030.firebaseapp.com',
+  databaseURL: 'https://api-project-440315309030.firebaseio.com',
+  projectId: 'api-project-440315309030',
+  storageBucket: 'gs://api-project-440315309030.appspot.com'
+}
